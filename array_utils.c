30cdc7934a87380c1576cc917387d1dfaf99c5ac
alimustafa <alimustafa2522@gmail.com>
2023-12-22 04:53:26 +0500

    reverse.c

--- array_utils.c
+++ array_utils.c
@@ -47,7 +47,12 @@ int *paddedCopy(const int *arr , int oldSize , int newsize)
 
 void reverse(int *arr , int size)
 {
-    
+    for(int i = 0 ; i<size/2; i++)
+    {
+        int temp = arr[i];
+        arr[i] = arr[size-1-i];
+        arr[size - 1 -i] = temp;
+    }
 }
 
 
--- array_utils.h
+++ array_utils.h
@@ -2,5 +2,5 @@
 int contains(const int *arr , int size , int x);
 int containsWithin(const int *arr , int size , int x , int i , int j)
 int *paddedCopy(const int *arr , int oldSize , int newsize);
-
+void reverse(int *arr , int size)
 //place your documentation and prototypes here
